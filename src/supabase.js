d45ac87e9dbb501219b450bad510b38e12d9746f
sfroysbr-coder <sfroysbr@gmail.com>
2026-05-10 13:21:28 -0400

    Delete src directory

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://kctjpejkqufndlbchuyr.supabase.co',
-  'sb_publishable_kVGAEEcLi6d4Gxyzmy1FDQ_OL4JaqHN'
-);
